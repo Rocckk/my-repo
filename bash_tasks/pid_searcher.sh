f3c967c61a36f8fc91b21f7977032afdcaeb4b39
Igor <itymos@vm.freebsd.com>
2019-02-06 15:55:22 +0200

    modified pis searcher to accept optional argument with the user name

--- bash_tasks/pid_searcher.sh
+++ bash_tasks/pid_searcher.sh
@@ -1,5 +1,13 @@
 #!/usr/local/bin/bash
-
-echo "the PIDs of the processes run by user $(whoami) are:"
-
-ps -U itymos | sed '1 d' | cut -c '1-5'
+if [ $1 ]; then
+    # here we direct stdout and stderr to the data sink to suppress standard behavior of the commmand
+    if ps -U $1 &> /dev/null; then
+        echo "the PIDs of the processes run by user $1 are:"
+        ps -U $1 | sed '1 d' | cut -c '1-5'
+    else
+        echo "There are npo processes run by this user!"
+    fi
+else
+    echo "the PIDs of the processes run by user $(whoami) are:"
+    ps -U $(whoami) | sed '1 d' | cut -c '1-5'
+fi
