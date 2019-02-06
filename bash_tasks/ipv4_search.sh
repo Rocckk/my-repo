2eb644fdb341f00ac7e58fcd8cfcf27054c36e04
Igor <itymos@vm.freebsd.com>
2019-02-06 17:11:49 +0200

    wrote  all the used regexes as constants

--- bash_tasks/ipv4_search.sh
+++ bash_tasks/ipv4_search.sh
@@ -1,9 +1,11 @@
 #!/usr/local/bin/bash
 
+PATTERN="([^[:punct:]]|^)\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b([^[:punct:]]|$)"
+
 echo "ipv4 addresses will be checked now!"
 
 
 
 echo -e "this is optimal solution:\nthe results are valid IPS, the ambiguous results (sequences of numbers, numbers and letters are excluded; \nmatches separated from other characters with any punctuation marks are excluded to provide a clearer result)"
 
-egrep -o  "([^[:punct:]]|^)\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b([^[:punct:]]|$)" $1 | tr -d ' '
+egrep -o $PATTERN $1 | tr -d ' '
--- bash_tasks/time_search.sh
+++ bash_tasks/time_search.sh
@@ -1,4 +1,6 @@
 #!/usr/local/bin/bash
+
+PATTERN="([0-1][0-9]|[2][0-4]):[0-5][0-9]:[0-5][0-9]"
  echo "$ is $1"
-echo "the current time in format hh:mm:ss is:"
-egrep -wo "([0-1][0-9]|[2][0-4]):[0-5][0-9]:[0-5][0-9]" $1
+echo "the time in format hh:mm:ss is:"
+egrep -wo $PATTERN  $1
