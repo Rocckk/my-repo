ea4f59534a98e6292a78c4035cb48c941eb33f78
Igor <itymos@vm.freebsd.com>
2019-04-10 17:43:43 +0300

    reading mails

--- python_tasks_ss/yuri_task/mail_reader.py
+++ python_tasks_ss/yuri_task/mail_reader.py
@@ -1,37 +1,50 @@
 #  прочитати файл, в унікоді погратися, зберегти в коремому файлі зміст.
 
 from email.parser import BytesParser
-from email.policy import default
+from email.policy import default, HTTP, SMTP
+from email import message_from_file
 import locale
 import sys
 
 
+
+#  using convenience functions
+with open("mail1", "r", encoding="utf-8") as f:
+    cont = message_from_file(f, policy=default)
+    print('start:----')
+    print(cont)
+    print('stop:----')
+    with open("targ.txt", 'w', encoding="utf-8")as fp:
+        fp.write(cont.as_string(maxheaderlen=90))
+
 """
-with open("mail2", "rb") as f:
+#  reading in binary mode
+with open("mail1", "rb") as f:
     cont = BytesParser(policy=default).parse(f)
 
 for i in cont.walk():
-    #print(i.get_content_type())
-    with open("targ.txt", 'ab') as fp:
-        if i.get_content_type() == "text/plain":
-            print (i)
+    if i.get_content_type() == "text/plain":
+        with open("targ.txt", 'wb') as fp:  
             fp.write(i.get_payload(decode=True))
-
+"""
+'''
+# reading message's body withou headers
 body = cont.get_body(preferencelist=('plain'))
 mes = body.get_content()
 with open("targ.txt", 'w', encoding="utf-8") as fp:
    fp.write(mes)
-
- """
-
-with open("mail2", "r", encoding="cp1251") as f:
+'''
+"""
+# simple reading and writing
+with open("t.txt", "r", encoding="utf-8") as f:
     a = f.read()
-    with open("targ.txt", 'w', encoding="cp1251") as fp:
+    with open("targ.txt", 'w', encoding="utf-8", newline='\r\n') as fp:
         fp.write(a)
 print(locale.getpreferredencoding())
+"""
 
-
-'''
+"""
+#  loop for char comparison
 with open("mail2", "r", encoding="utf-8") as f:
     a = f.read()
     print(len(a))
@@ -42,5 +55,5 @@ with open("mail2", "r", encoding="utf-8") as f:
             if a[i] != b[i]:
                 print('!!!!!!!!!!!!!')
                 break
-'''
+"""
 
