6f3e2b164cc1f7575b69a062f484f62adbc8b2b4
Igor <itymos@vm.freebsd.com>
2019-04-25 14:24:34 +0300

    the project with scrapy almos done

--- python_tasks_ss/url_getter_project/urler/urler/pipelines.py
+++ python_tasks_ss/url_getter_project/urler/urler/pipelines.py
@@ -5,7 +5,72 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from datetime import datetime
+import pymysql
+from .creds import USER, PASSWORD, DB
 
-class UrlerPipeline(object):
+
+class UrlerMysqlPipeline(object):
+
+
+    def open_spider(self, spider):
+        print('$$$', object)
+        self.connection = pymysql.connect(host='localhost', user=USER,
+                                     password=PASSWORD, db=DB)
+
+        webpage = spider.start_urls[0]
+        webpage_count = self.check_presence(webpage)
+        if webpage_count == 1:
+            date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+            self.insert_webpage(webpage, date, webpage_count)
+        else:
+            date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+            self.update_webpage(webpage, date, webpage_count)
+
+    def close_spider(self, spider):
+        self.connection.close()
+        print(30*'---')
+        print('closed')
+
+
     def process_item(self, item, spider):
+        webpage = spider.start_urls[0]
+        self.insert_url(webpage, item['url'], item['count'])
         return item
+
+    def insert_webpage(self, webpage, date, count):
+        with self.connection.cursor() as cursor:
+            if cursor.execute("insert into `webpages` (`url`, `time_crawled`,\
+`crawl_count`) values ('{}', '{}', {})".format(webpage, date, str(count))):
+                self.connection.commit()
+            else:
+                print('insertion failed!')
+
+    def check_presence(self, url):
+        with self.connection.cursor() as cursor:
+            present = cursor.execute("select * from `webpages` where `url` =\
+'{}'".format(url))
+            if present:
+                count = cursor.fetchone()
+                return count[3] + 1
+            return 1
+
+    def update_webpage(self, webpage, date, count):
+        with self.connection.cursor() as cursor:
+            if cursor.execute("update `webpages` set `time_crawled` =\
+'{}', `crawl_count` = {} where `url` = '{}'".format(date, count, webpage)):
+                self.connection.commit()
+            else:
+                print('update failed')
+
+    def insert_url(self, webpage, url, count):
+        with self.connection.cursor() as cursor:
+            if cursor.execute("insert into `scraped_urls` (`webpage_id`, `url`,\
+`count_on_page`) values ((select `id` from `webpages` where `url` = '{}'), \
+'{}', {})".format(webpage, url, str(count))):
+                self.connection.commit()
+            else:
+                print('insertion url failed!')
+
+
+
--- python_tasks_ss/url_getter_project/urler/urler/spiders/spider_url.py
+++ python_tasks_ss/url_getter_project/urler/urler/spiders/spider_url.py
@@ -18,17 +18,31 @@ class UrlSpider(scrapy.Spider):
                            'pluginspage', 'action', 'profile', 'manifest',
                            'xmlns', 'lowsrc', 'classid', 'data', 'itemtype')
         self.pattern = r"https?://[^\s,\'\"\)\(\}\{\<]+\.+[^\s,\'\"\)\(\}\{\<]+"
-        
-        self.extractor = LinkExtractor(allow=(self.pattern), process_value=self.regex_link,
+        self.tag_attr_combos = ('a[href]', 'applet[archive]', 'applet[codebase]',
+                               'base[href]', 'bgsound[src]', 'blockquote[cite]',
+                               'body[background]', 'button[formaction]',
+                               'command[icon]', 'del[cite]', 'div[itemtype]',
+                               'embed[src]', 'form[action]', 'frame[src]',
+                               'head[profile]', 'html[manifest]', 'html[xmlns]',
+                               'iframe[src]', 'img[src]', 'input[formaction]',
+                               'input[src]', 'ins[cite]', 'isindex[action]',
+                               'link[href]', 'meta[itemtype]',
+                               'object[archive]', 'object[codebase]', 'q[cite]',
+                               'source[src]', 'svg[itemtype]',
+                               'table[background]', 'td[background]',
+                               'th[background]', 'video[src]',
+                               'embed[pluginspage]', 'img[lowsrc]',
+                               'object[classid]', 'object[data]')
+        self.extractor = LinkExtractor(process_value=self.regex_link,
                                        tags=self.link_tags,
-                                       attrs=self.link_attrs, unique=False)
-         
+                                       attrs=self.link_attrs, unique=False,
+                                       restrict_css=self.tag_attr_combos)
 
     def regex_link(self, value):
         match = re.search(self.pattern, value)
         if match:
             return match.group()
-    
+
     def parse(self, response):
         list_of_links = [i.url for i in self.extractor.extract_links(response)]
         text_ls = [i.text for i in self.extractor.extract_links(response) if
@@ -44,9 +58,12 @@ class UrlSpider(scrapy.Spider):
                        i = i.replace(match.group(), '', 1)
                 else:
                     i = i.replace('http', '', 1)
-        print('##', list_of_links, len(list_of_links))
-        #for i in enumerate(list_of_links):
-        #    yield {'url' + str(i[0]): i[1]}
+
+        #rint('##', list_of_links, len(list_of_links))
+        no_duplicate_list = filter(lambda el: list_of_links.count(el) == 1,
+                list_of_links)
+        for i in no_duplicate_list:
+            yield {'url' : i, 'count': list_of_links.count(i)}
 
 
 
