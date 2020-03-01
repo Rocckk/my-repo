56f97e7fa5991cc78b3dd48327b738f167641312
Rocckk <igorrrock.it@gmail.com>
2020-03-02 00:46:06 +0200

    created crawler

---
+++ n-ix_crawler/github_crawler.py
@@ -0,0 +1,165 @@
+"""
+This module implements a Github web crawler, which accepts a standardized json input which is read
+from a json file that is piped to the script as the standard input (stdin), e.g:
+>>python github_crawler.py < input.json
+
+The input file should contain 3 keys: keywords, proxies, type.
+The value of 'keywords' key should be an array of 3 keywords to search for on Github:
+"keywords": ["python", "vagrant", "django"]
+
+The value of 'proxies' key should be an array of 2 proxy server's URLs with their schemas
+included:
+"proxies": ["http://80.255.91.38:43360", "https://118.97.235.234:8080"]
+
+The value of 'type' key should be an string with one of the following 3 values: 'Repositories',
+'Issues', 'Wikis', which indicates search sections of Github to search through:
+"type": "Issues"
+
+The crawler searches for the keywords on Github and returns a json file with the result (URLs which
+match search criteria). For types 'Issues' and 'Wikis' the result will have the following schema:
+[
+    {
+        "url": "https://github.com/andreagrandi/covid-api/issues/1"
+    },
+    {
+        "url": "https://github.com/Prefeitura-Comunica/thread/issues/3"
+    }
+]
+For Repositories, it will additionally include information on a repo owner and languages used for
+the repo. The schema is as follows:
+[
+    {
+        "url": "https://github.com/davidfischer-ch/ansible-roles",
+        "extra": {
+            "owner": "davidfischer-ch",
+            "language_stats": {
+                "Python": 99.9,
+                "Shell": 0.1
+            }
+        }
+    }
+]
+"""
+import concurrent.futures
+import json
+import sys
+
+from lxml.html import fromstring
+
+import requests
+
+OBJECT_TYPES = ['Repositories', 'Issues', 'Wikis']
+
+
+def get_input(content):
+    """
+    This function accepts a file-like object (stdin) with json input and extracts necessary data
+    from it
+    :param content: file-like object with input
+    :return: tuple containing proxies to use for requests, keywords to search for and type of
+    Github section to search for.
+    """
+    obj = json.loads(content)
+    proxies = {i.split('://')[0]: i for i in obj.get('proxies')}
+    keywords = obj.get('keywords')
+    obj_type = obj.get('type')
+    return proxies, keywords, obj_type
+
+
+def parse_repo(url, proxies):
+    """
+    This function sends requests to repo URL, parses it and extracts necessary data
+    :param url: string, the URL of Github repo
+    :param proxies: dict, dict where key is the schema and value is the actual URL of a proxy
+    {'https': 'https://81.33.4.214:61711', 'http': 'http://185.176.32.160:3128'}
+
+    :return: dict, dict containing the URL of the page, the owner of the repo and repo's language
+    statistics
+    """
+    r = requests.get(url, proxies=proxies)
+    result_html = fromstring(r.text)
+    langs = [
+        i.get('aria-label') for i in result_html.cssselect(
+            'div.d-flex.repository-lang-stats-graph span'
+        )
+    ]
+    owner = result_html.cssselect('span.author.ml-1.flex-self-stretch[itemprop=author] a')[0]
+    return {
+        'url': url,
+        'extra': {
+            'owner': owner.text_content(), 'language_stats': {
+                ' '.join(i.split()[:-1]): float(i.split()[-1].rstrip('%')) for i in langs
+            }
+        }
+    }
+
+
+def get_urls(keywords, proxies, obj_type):
+    """
+    This function retrieves URLs matching search keywords passsed as the input
+    :param keywords: list, the list of keywords to search for
+    :param proxies: dict, dict where key is the schema and value is the actual URL of a proxy
+    {'https': 'https://81.33.4.214:61711', 'http': 'http://185.176.32.160:3128'}
+    :param obj_type: string, the type of Github section to search through
+    :return: list, list of dictionaries where key is always 'url' and value is the URL of the page
+    which is suitable
+    """
+    if obj_type == OBJECT_TYPES[0]:
+        github_url = 'https://github.com/search?utf8=%E2%9C%93&q=topic%3A{}+topic%3A{}+topic%3A{}&\
+ref=simplesearch'.format(keywords[0], keywords[1], keywords[2])
+    elif obj_type == OBJECT_TYPES[1]:
+        github_url = 'https://github.com/search?q={}+{}+{}+type%3Aissue'.format(
+            keywords[0], keywords[1], keywords[2]
+        )
+    elif obj_type == OBJECT_TYPES[2]:
+        github_url = 'https://github.com/search?q={}+{}+{}&type=Wikis'.format(
+            keywords[0], keywords[1], keywords[2]
+        )
+    r = requests.get(url=github_url, proxies=proxies)
+    result_html = fromstring(r.text)
+    urls = [
+        {'url': 'https://github.com' + i.get('href')} for i in result_html.cssselect(
+            'div.f4.text-normal a'
+        )
+    ]
+    return urls
+
+
+def get_extra(urls, obj_type, proxies):
+    """
+    This function concurrently sends requests to Github repo URLs passed as its parameter and
+    returns the structured result containing the URL, owner and language stats for this URL
+    :param urls: list, the array of dicts of URLs of Github repos
+    :param obj_type: string, the type of Github section to search through
+    :param proxies: dict, dict where key is the schema and value is the actual URL of a proxy
+    {'https': 'https://81.33.4.214:61711', 'http': 'http://185.176.32.160:3128'}
+    :return: list, the list of dicts containing the URL of the page, the owner of the repo and
+    repo's language statistics
+    """
+    if obj_type == OBJECT_TYPES[0]:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(urls)) as executor:
+            future_to_url = {
+                executor.submit(parse_repo, url.get('url'), proxies): url.get('url') for url in urls
+            }
+            results = [future.result() for future in concurrent.futures.as_completed(future_to_url)]
+            return results
+
+
+def write_results(results):
+    with open('result.json', 'w') as f:
+        json.dump(results, f, ensure_ascii=False, indent=4)
+
+
+def main():
+    content = sys.stdin.read()
+    proxies, keywords, obj_type = get_input(content)
+    urls = get_urls(keywords, proxies, obj_type)
+    results = get_extra(urls, obj_type, proxies)
+    if results:
+        write_results(results)
+    else:
+        write_results(urls)
+
+
+if __name__ == '__main__':
+    main()
---
+++ n-ix_crawler/tests.py
@@ -0,0 +1,212 @@
+import json
+import sys
+import unittest
+
+from github_crawler import get_extra, get_input, get_urls, main, parse_repo, write_results
+
+
+class TestCrawlerWikis(unittest.TestCase):
+    def setUp(self):
+        self.keywords = ["python", "redis", "django"]
+        self.proxies = {'https': 'https://81.33.4.214:61711', 'http': 'http://185.176.32.160:3128'}
+        self.obj_type = "Wikis"
+        self.urls = get_urls(self.keywords, self.proxies, self.obj_type)
+        self.result_wikis = [
+            {
+                "url": "https://github.com/balakrishnanm/mybook/wiki/Python-Django"
+            },
+            {
+                "url": "https://github.com/alibond/appunti.italiadigitale.docs/wiki/Django"
+            },
+            {
+                "url": "https://github.com/victorhooi/victorwiki/wiki/django"
+            },
+            {
+                "url": "https://github.com/kailiu-unsw/python/wiki/Django"
+            },
+            {
+                "url": "https://github.com/cfbatalla/LenguajesDeAltoNivel/wiki/Django"
+            }
+        ]
+
+    def test_get_urls_return_type_wikis(self):
+        self.assertIsInstance(self.urls, list)
+
+    def test_get_urls_return_member_type_wikis(self):
+        if len(self.urls) > 0:
+            self.assertIsInstance(self.urls[0], dict)
+
+    def test_get_urls_return_member_type_keys_wikis(self):
+        if len(self.urls) > 0:
+            self.assertIn('url', self.urls[0])
+
+    def test_get_urls(self):
+        urls = get_urls(self.keywords, self.proxies, self.obj_type)
+        self.assertTrue(urls)
+
+    def test_write_results(self):
+        write_results(self.result_wikis)
+        with open('result.json', 'r') as f:
+            contents = f.read()
+            self.assertEqual(json.loads(contents), self.result_wikis)
+
+    def test_get_input_type(self):
+        with open('input.json', 'r') as f:
+            content = f.read()
+            self.assertIsInstance(get_input(content), tuple)
+
+    def test_get_input_len(self):
+        with open('input.json', 'r') as f:
+            content = f.read()
+            self.assertEqual(len(get_input(content)), 3)
+
+    def test_main(self):
+        with open('input.json', 'r') as f:
+            sys.stdin = f
+            self.assertEqual(main(), None)
+
+
+class TestCrawlerRepos(unittest.TestCase):
+    def setUp(self):
+        self.keywords = ["python", "redis", "django"]
+        self.proxies = {'https': 'https://81.33.4.214:61711', 'http': 'http://185.176.32.160:3128'}
+        self.obj_type = "Repositories"
+        self.urls = get_urls(self.keywords, self.proxies, self.obj_type)
+        self.result_repos = [
+            {
+                "url": "https://github.com/fabiocaccamo/django-freeze",
+                "extra": {
+                    "owner": "fabiocaccamo",
+                    "language_stats": {
+                        "Python": 94.7,
+                        "HTML": 3.3,
+                        "JavaScript": 2.0
+                    }
+                }
+            },
+            {
+                "url": "https://github.com/Anupam-dagar/Portfolio-Generator",
+                "extra": {
+                    "owner": "Anupam-dagar",
+                    "language_stats": {
+                        "Python": 56.7,
+                        "JavaScript": 27.7,
+                        "HTML": 15.6
+                    }
+                }
+            },
+            {
+                "url": "https://github.com/justdjango/Shopping_cart",
+                "extra": {
+                    "owner": "justdjango",
+                    "language_stats": {
+                        "JavaScript": 48.3,
+                        "CSS": 30.1,
+                        "Python": 12.8,
+                        "HTML": 8.8
+                    }
+                }
+            }
+        ]
+
+    def test_get_url_return_type_repos(self):
+        self.assertIsInstance(self.urls, list)
+
+    def test_get_url_return_member_type_repos(self):
+        if len(self.urls) > 0:
+            self.assertIsInstance(self.urls[0], dict)
+
+    def test_get_url_return_member_type_has_key_urls(self):
+        if len(self.urls) > 0:
+            self.assertIn('url', self.urls[0])
+
+    def test_parse_repo_result_type(self):
+        if self.urls:
+            result = parse_repo(self.urls[0].get('url'), self.proxies)
+            self.assertIsInstance(result, dict)
+
+    def test_parse_repo_result_has_key_urls(self):
+        if self.urls:
+            result = parse_repo(self.urls[0].get('url'), self.proxies)
+            self.assertIn('url', result)
+
+    def test_parse_repo_result_has_key_extra(self):
+        if self.urls:
+            result = parse_repo(self.urls[0].get('url'), self.proxies)
+            self.assertIn('extra', result)
+
+    def test_parse_repo_result_extra_has_key_owner(self):
+        if self.urls:
+            result = parse_repo(self.urls[0].get('url'), self.proxies)
+            self.assertIn('owner', result.get('extra'))
+
+    def test_parse_repo_result_extra_has_key_language_stats(self):
+        if self.urls:
+            result = parse_repo(self.urls[0].get('url'), self.proxies)
+            self.assertIn('language_stats', result.get('extra'))
+
+    def test_get_extra_return_type(self):
+        results = get_extra(self.urls, 'Repositories', self.proxies)
+        self.assertIsInstance(results, list)
+
+    def test_get_extra_return_member_type(self):
+        results = get_extra(self.urls, 'Repositories', self.proxies)
+        if len(results) > 0:
+            self.assertIsInstance(results[0], dict)
+
+    def test_get_extra_result_has_key_urls(self):
+        results = get_extra(self.urls, 'Repositories', self.proxies)
+        self.assertIn('url', results[0])
+
+    def test_get_extra_result_has_key_extra(self):
+        results = get_extra(self.urls, 'Repositories', self.proxies)
+        self.assertIn('extra', results[0])
+
+    def test_get_extra_result_extra_has_key_owner(self):
+        results = get_extra(self.urls, 'Repositories', self.proxies)
+        self.assertIn('owner', results[0].get('extra'))
+
+    def test_get_extra_result_extra_has_key_language_stats(self):
+        results = get_extra(self.urls, 'Repositories', self.proxies)
+        self.assertIn('language_stats', results[0].get('extra'))
+
+    def test_write_results(self):
+        write_results(self.result_repos)
+        with open('result.json', 'r') as f:
+            contents = f.read()
+            self.assertEqual(json.loads(contents), self.result_repos)
+
+
+class TestCrawlerIssues(unittest.TestCase):
+    def setUp(self):
+        self.keywords = ["python", "redis", "django"]
+        self.proxies = {'https': 'https://81.33.4.214:61711', 'http': 'http://185.176.32.160:3128'}
+        self.obj_type = "Issues"
+        self.urls = get_urls(self.keywords, self.proxies, self.obj_type)
+        self.result_issues = [
+            {"url": "https://github.com/andreagrandi/covid-api/issues/1"},
+            {"url": "https://github.com/Prefeitura-Comunica/thread/issues/3"},
+            {"url": "https://github.com/deity-io/falcon/issues/770"},
+            {"url": "https://github.com/zgq105/blog/issues/80"}
+        ]
+
+    def test_get_url_issues(self):
+        self.assertIsInstance(self.urls, list)
+
+    def test_get_url_member_type_issues(self):
+        if len(self.urls) > 0:
+            self.assertIsInstance(self.urls[0], dict)
+
+    def test_get_url_return_member_type_keys_issues(self):
+        if len(self.urls) > 0:
+            self.assertIn('url', self.urls[0])
+
+    def test_write_results(self):
+        write_results(self.result_issues)
+        with open('result.json', 'r') as f:
+            contents = f.read()
+            self.assertEqual(json.loads(contents), self.result_issues)
+
+
+if __name__ == '__main__':
+    unittest.main()
